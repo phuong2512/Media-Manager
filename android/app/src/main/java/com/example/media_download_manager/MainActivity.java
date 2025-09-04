13fa30f3dfde9e412b0be73a08b76fe501225e80
Trần Văn Phương <99707516+phuong2512@users.noreply.github.com>
2025-09-04 10:39:57 +0700

    Initial commit

---
+++ android/app/src/main/java/com/example/media_download_manager/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.media_download_manager;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/views/home/home_screen.dart
@@ -0,0 +1,109 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:media_download_manager/views/home/home_list_tile.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List demoAudioList = [
+    {
+      "name": "Audio 1",
+      "path": "audio/audio1.mp3",
+      "duraion": "05:00",
+      "size": 10,
+    },
+    {
+      "name": "Audio 2",
+      "path": "audio/audio2.mp3",
+      "duraion": "0:50",
+      "size": 15,
+    },
+    {
+      "name": "Audio 3",
+      "path": "audio/audio3.mp3",
+      "duraion": "03:05",
+      "size": 20,
+    },
+  ];
+  List demoVideoList = [
+    {
+      "name": "Video 1",
+      "path": "video/video1.mp4",
+      "duraion": "05:00",
+      "size": 10,
+    },
+    {
+      "name": "Video 2",
+      "path": "video/video2.mp4",
+      "duraion": "0:50",
+      "size": 15,
+    },
+    {
+      "name": "Video 3",
+      "path": "video/video3.mp4",
+      "duraion": "03:05",
+      "size": 20,
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Media Loader'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.settings, color: Color(0xFF215B9D)),
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                HomeListTile(mediaType: 'Audio', mediaList: demoAudioList,),
+                HomeListTile(mediaType: 'Video', mediaList: demoVideoList,),
+              ],
+            ),
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: Container(
+        width: 56,
+        height: 56,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          gradient: const LinearGradient(
+            colors: [Colors.blue, Colors.purple],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withValues(alpha: 0.3),
+              blurRadius: 6,
+              offset: const Offset(0, 3),
+            ),
+          ],
+        ),
+        child: IconButton(
+          icon: const Icon(Icons.add, color: Colors.white),
+          onPressed: _addMediaToHome,
+        ),
+      ),
+    );
+  }
+
+  void _addMediaToHome() async {}
+
+}
